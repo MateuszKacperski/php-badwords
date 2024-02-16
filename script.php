c5acec3f4e2ab758ce4531d1e1de4f8db1aaf1cd
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-16 16:43:23 +0100

    form structure

--- script.php
+++ script.php
@@ -1,4 +1,14 @@
 <?php
 
+$paragraph = $_GET["paragraph"];
+$censured = $_GET["censured"];
 
+echo $paragraph;
+echo strlen($paragraph);
+
+
+
+$changed_p = str_replace($censured,"***", $paragraph);
+echo $changed_p;
+echo strlen($changed_p);
 ?>
